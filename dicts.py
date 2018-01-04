55149e10158a55832d26cb0dfb7281fba6995e73
Victor Cassen <phonybone@gmail.com>
2018-01-04 13:21:03 -0800

    dicts.py: adding hashsubset

--- dicts.py
+++ dicts.py
@@ -10,3 +10,6 @@ def hashtuple(d, *keys):
 def to_dict(config, section):
     ''' return the section of config as a dict '''
     return {k:v for k,v in config.items(section)}
+
+def hashsubset(d, *keys):
+    return {k:d[k] for k in keys}
