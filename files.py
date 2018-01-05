7a2e2bd28417ce43b49683b1105f9075fae25745
Victor Cassen <phonybone@gmail.com>
2018-01-05 11:29:21 -0800

    files.py: added replace_ext()

--- files.py
+++ files.py
@@ -24,7 +24,22 @@ def get_size(path, units='auto'):
             
     return '{}{}'.format(size, units.capitalize())
 
+def replace_ext(path, new_ext):
+    ''' 
+    replace the extension on a path with a new extension.  Replacement occurs
+    even if there was no extension on the original path.
+    '''
+    head, tail = os.path.splitext(path)
+    return head + '.' + new_ext
+
+
 if __name__=='__main__':
     path = '/usr/local/everest/data/var/UserData/Victor/Reads/LB5025/LB5025germline_PG0-718_LB4852V_rename.fastq'
-    for units in 'auto bytes kb mb gb pb'.split(' '):
-        print 'answer: {}\n'.format(get_size(path, units))
+    # for units in 'auto bytes kb mb gb pb'.split(' '):
+    #     print 'answer: {}\n'.format(get_size(path, units))
+
+    print replace_ext(path, 'fq')
+    print replace_ext('/usr/local/bin', 'txt')
+    print replace_ext('/usr/local/bin/fart.html', 'txt')
+    print replace_ext('/usr/local/bin/fart.orig.html', 'txt')
+    print replace_ext('/', '.bin')
