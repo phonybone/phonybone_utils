6cada12c4d65b7e8e8c912c3e908243fd5138ff6
Victor Cassen <phonybone@gmail.com>
2018-01-12 13:07:44 -0800

    python.py: add @configured_class()

--- python.py
+++ python.py
@@ -1,7 +1,10 @@
 '''
 Utility functions related to general-purpose python.
 '''
+import os
 import importlib
+from files import package_fileres
+from configs import get_config, load_attributes_from_config, to_dict
 
 def lazy(fn):
     attr_name = '__lazy_' + fn.__name__
@@ -19,6 +22,29 @@ def import_class(fullname):
     mod = importlib.import_module(modname)
     return getattr(mod, clsname)
 
+class configured_class(object):
+    def __init__(self, defaults_fn=None, def_section=None):
+        self.defaults_fn = defaults_fn
+        self.def_section = def_section
+        
+
+    def __call__(self, cls):
+        clsname = cls.__name__  # shorthand
+        config_fn = package_fileres(clsname.lower(), clsname.lower()+'.ini')
+
+        # try to get default values from default config and section:
+        if self.defaults_fn is not None and self.def_section is not None:
+            if not os.path.isabs(self.defaults_fn):
+                self.defaults_fn = package_fileres(clsname.lower(), self.defaults_fn)            
+            def_config = get_config(self.defaults_fn)
+            defaults = to_dict(def_config, self.def_section)
+        else:
+            defaults = {}
+        cls_config = get_config(config_fn, defaults=defaults)
+
+        load_attributes_from_config(cls, cls_config, clsname.lower())
+        return cls
+        
 
 if __name__ == '__main__':
     def test_lazy():
