e156658a4484d654a7292dbffa7198040c2b2f2a
Victor Cassen <phonybone@gmail.com>
2018-01-12 15:14:32 -0800

    python.configured_class: updates to defaults handling

--- python.py
+++ python.py
@@ -3,7 +3,8 @@ Utility functions related to general-purpose python.
 '''
 import os
 import importlib
-from files import package_fileres
+import inspect
+from files import package_fileres, replace_ext
 from configs import get_config, load_attributes_from_config, to_dict
 
 def lazy(fn):
@@ -23,26 +24,41 @@ def import_class(fullname):
     return getattr(mod, clsname)
 
 class configured_class(object):
+    '''
+    Decorator to set class attributes from a config section(s).
+    With no parameters, looks for a config (.ini) file with the
+    name <class>.ini, where <class> is the name of the class 
+    (converted to lowercase). From within that config, it looks 
+    for a section with the (exact) name of the class from which
+    to initialize values.
+
+    An addition config and section can be specified using the
+    arguments to the decorator.  If given, it looks for file 
+    relative to the class's package (unless the filename given
+    is an absolute path) and uses the section specified.
+    '''
     def __init__(self, defaults_fn=None, def_section=None):
         self.defaults_fn = defaults_fn
         self.def_section = def_section
         
 
     def __call__(self, cls):
+        clsfile = inspect.getfile(cls)
+        config_fn = replace_ext(os.path.basename(clsfile), 'ini')
         clsname = cls.__name__  # shorthand
-        config_fn = package_fileres(clsname.lower(), clsname.lower()+'.ini')
 
         # try to get default values from default config and section:
         if self.defaults_fn is not None and self.def_section is not None:
             if not os.path.isabs(self.defaults_fn):
-                self.defaults_fn = package_fileres(clsname.lower(), self.defaults_fn)            
+                modname, _ = os.path.splitext(os.path.basename(clsfile))
+                self.defaults_fn = package_fileres(modname, self.defaults_fn)            
             def_config = get_config(self.defaults_fn)
             defaults = to_dict(def_config, self.def_section)
         else:
             defaults = {}
         cls_config = get_config(config_fn, defaults=defaults)
 
-        load_attributes_from_config(cls, cls_config, clsname.lower())
+        load_attributes_from_config(cls, cls_config, clsname)
         return cls
         
 
