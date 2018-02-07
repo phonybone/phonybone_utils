f171f417a3160edb9dfdf0933e129d643233d14d
Victor Cassen <phonybone@gmail.com>
2018-02-06 16:53:34 -0800

    moving merge_overlap() to span_mixin.py

--- lists.py
+++ lists.py
@@ -17,21 +17,6 @@ def flatten(l):
     # see https://stackoverflow.com/questions/10632839/python-transform-list-of-tuples-in-to-1-flat-list-or-1-matrix
     return list(sum(l, ()))
 
-def merge_overlapping(spans, ovlp, mrg):
-    assert callable(ovlp)
-    assert callable(mrg)
-
-    answer = set()
-    for span in spans:
-        to_rm = [blob for blob in answer if ovlp(span, blob)]
-        for blob in to_rm:
-            span = mrg(span, blob)
-            answer.remove(blob)
-        answer.add(span)
-    return answer
-            
-        
-
 if __name__ == '__main__':
     from strings import ppjson, qw
     def test_grow_to():
@@ -56,35 +41,10 @@ if __name__ == '__main__':
         assert l6 is lol[5]
 
     def test_merge_overlapping():
-        
-        def ovlp(s1, s2):
-            # ensure order
-            if s1[0] > s1[1]:   
-                s1 = (s1[1], s1[0])
-            if s2[0] > s2[1]:
-                s2 = (s2[1], s2[0])
-            # do comparisons:
-            if (s2[0] <= s1[0] and s2[1] >= s1[0]) or (s2[0] <= s1[1] and s2[1] >= s1[1]):
-                return True
-            if (s1[0] >= s2[0] and s1[1] <= s2[1]) or (s1[0] <= s2[0] and s1[1] >= s2[1]):
-                return True
-            return False
 
-        # verify correctness of ovlp
-        s1 = (10, 20)
-        s2s = ((1,5), (5,10), (11,12), (18, 22), (23,38), (9, 21))
-        exp = [False, True, True, True, False, True]
-        answer = [ovlp(s1, s2) == exp[i] for i,s2 in enumerate(s2s)]
-        assert all(answer)
-        
-        s2s = ((5,1), (10,5), (12,11), (22, 18), (38,23), (21, 9)) # reverse orders
-        answer = [ovlp(s1, s2) == exp[i] for i,s2 in enumerate(s2s)]
-        assert all(answer)
-
-        def mrg(s1, s2):
-            return (min(s1[0], s2[0]), max(s1[1], s2[1]))
         
         data = (
+            ((1, 5), (4, 8)),
             ((23, 34), (43, 48), (48, 52), (51, 59), (62, 67)),
             ((23, 34),),
             ((1,3),(3,1)),
@@ -92,6 +52,7 @@ if __name__ == '__main__':
             ((3,4), (1,6)),
             )
         expected = (
+            ((1, 8),),
             ((23, 34), (43, 59), (62, 67)),
             ((23, 34),),
             ((1,3),),
