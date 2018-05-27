04d43cd0b337d3fb577aa0127a625f7ff7af56f1
Victor Cassen <victor@statelessnetworks.com>
2018-05-27 13:32:26 -0700

    updating tests for sqla and mysql_utils

--- pbutils/tests/sqla/test_init.py
+++ pbutils/tests/sqla/test_init.py
@@ -1,10 +1,11 @@
 import unittest
 import pkg_resources as pr
+from sqlalchemy.orm.session import sessionmaker
 
 from pbutils.sqla import sqlA_init_from_config_fn
 from pbutils.configs import get_config
 from pbutils.mysql_utils import get_mysql
-from pbutils.strings import qw
+from pbutils.strings import qw, ppjson
 from pbutils.tests import create_db
 
 class SqlaTest(unittest.TestCase):
@@ -12,11 +13,9 @@ class SqlaTest(unittest.TestCase):
         mysql_ini = pr.resource_filename('pbutils', 'tests/fixtures/mysql.ini')
         config = get_config(mysql_ini)
         database = config.get('mysql', 'database')
-        print('using database "{}"'.format(database))
         args = dict(config.items('mysql'))
         args['database'] = 'mysql'
         dbh = get_mysql(**args)
         create_db(dbh, database)
-        print('{} created'.format(database))
         Session = sqlA_init_from_config_fn(mysql_ini, 'mysql')
-        print('Session: {}'.format(Session))
+        self.assertEqual(type(Session), sessionmaker)
