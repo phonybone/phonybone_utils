af6e4034405ee1dd60d50912cb28fc4ec3470520
Victor Cassen <c.victor.cassen@zonarsystems.com>
2019-03-07 12:37:17 -0800

    dicts.py: added from_attrs()

--- pbutils/dicts.py
+++ pbutils/dicts.py
@@ -1,21 +1,34 @@
 import json
 
+
 def keystr(d, connector=', '):
     return connector.join(d.keys())
+
 
 def hashslice(d, *keys):
     return [d[k] for k in keys]
 
+
 def hashtuple(d, *keys):
     return tuple(hashslice(d, *keys))
 
+
 def to_dict(config, section):
     ''' return the section of config as a dict '''
-    return {k:v for k,v in config.items(section)}
+    return config.items(section)
+
 
 def hashsubset(d, *keys):
-    return {k:d[k] for k in keys}
+    return {k: d[k] for k in keys}
+
 
 def json_copy(d):
     ''' make a copy of raw data by converting to json and back (faster than copy.deepcopy for simple data) '''
     return json.loads(json.dumps(d))
+
+
+def from_attrs(obj, keys=None):
+    ''' return a dictionary based on an object's attributes '''
+    if keys is None:
+        keys = obj.__dict__.keys()
+    return hashsubset(obj.__dict__, *keys)
