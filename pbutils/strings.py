f12bb69708eb540673c2fb0caa5d518d292abf7c
Victor Cassen <c.victor.cassen@zonarsystems.com>
2019-03-07 14:34:23 -0800

    strings.py: (questionable?) upgrade of ppjson() to allow floating point precision in returned dump

--- pbutils/strings.py
+++ pbutils/strings.py
@@ -5,6 +5,7 @@ Random string utils.
 import re
 import json
 
+
 def expand_range_list(src):
     '''
     Given a string representing a list of integer-based ranges,
@@ -23,17 +24,18 @@ def expand_range_list(src):
     Note that although the example above is sorted, no such requirements are imposed or
     considered by this function.
     '''
-    
+
     # Strip all whitespace:
     src = re.sub(r'\s+', '', src)
     ranges = []
     for r in src.split(','):
         if '-' in r:
-            start,stop = map(int, r.split('-'))
-            ranges.extend(xrange(start, stop+1))
+            start, stop = map(int, r.split('-'))
+            ranges.extend(range(start, stop + 1))
         else:
             ranges.append(int(r))
     return ranges
+
 
 def chunks(s, ll):
     '''
@@ -63,12 +65,36 @@ class StringEncoder(json.JSONEncoder):
             return map(str, obj)
         try:
             return json.JSONEncoder.default(self, obj)
-        except:
+        except Exception:
             return str(obj)
 
-def ppjson(data, indent=2):
-    return json.dumps(data, indent=indent, cls=StringEncoder)
-    
+
+class PrettyFloat(float):
+    prec = 2
+    def __repr__(self):
+        return f'%.{self.prec}f' % self
+
+
+def pretty_floats(obj):
+    if isinstance(obj, float):
+        return repr(PrettyFloat(obj))
+    elif isinstance(obj, dict):
+        return dict((k, pretty_floats(v)) for k, v in obj.items())
+    elif isinstance(obj, (list, tuple)):
+        return list(map(pretty_floats, obj))
+    elif hasattr(obj, '__dict__'):
+        return pretty_floats(obj.__dict__)
+    return str(obj)
+
+
+def ppjson(data, indent=2, float_prec=2):
+    old_prec = PrettyFloat.prec
+    PrettyFloat.prec = float_prec
+    dump = json.dumps(pretty_floats(data), indent=indent)   # , cls=StringEncoder)
+    PrettyFloat.prec = old_prec
+    return dump
+
+
 def qw(s, rx=None):
     if s == '':
         return []
@@ -78,28 +104,38 @@ def qw(s, rx=None):
     else:
         return s.split(' ')
 
+
 if __name__ == '__main__':
     def test_ppjson():
         import datetime as dt
 
         class Foo(object):
             fmt = '%a, %m %d %Y'
-            def __init__(self, string, dob, z):
-                self.string = string
+
+            def __init__(self, str, dob, z, flt):
+                self.string = str
                 self.dob = dob
                 self.z = z
+                self.flt = flt
 
             def as_json(self):
                 return {
                     'string': self.string,
                     'dob': self.dob.strftime(self.fmt),
-                    'z': repr(self.z)
-                    }
+                    'z': repr(self.z),
+                    'flt': self.flt
+                }
 
+        foo = Foo('fred', dt.datetime.now(), 3+2j, 23.238932)
+        print('foo: {}'.format(ppjson(foo, float_prec=1)))
+        print(F"foo.__dict__: {foo.__dict__}")
+        print(F"foo.__dict__.pretty: {ppjson(foo.__dict__, float_prec=3)}")
+        print(F"foo.__dict__: {foo.__dict__}")
 
-        foo = Foo('fred', dt.datetime.now(), 3+2j)
-        print('foo: {}'.format(ppjson(foo)))
-    #test_ppjson()
+        print(ppjson([23.2390], float_prec=1))
+        for p in range(10):
+            print(ppjson(23.53424, float_prec=p))
+    test_ppjson()
 
     def test_qw():
         fodder = 'list, of, strings,   not,always, separated consistently'
@@ -108,5 +144,4 @@ if __name__ == '__main__':
         print(qw(fodder, re.compile('[\s,]+')))
         print(qw(fodder, re.compile('s')))
         print(qw(fodder, 's'))
-    test_qw()
-                 
+    # test_qw()
