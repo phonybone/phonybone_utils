41ef1848a2ff783f9ea92a9ba64e9f1bfe1b859f
Victor Cassen <c.victor.cassen@zonarsystems.com>
2019-04-04 09:06:12 -0700

    add tests/configs

---
+++ pbutils/tests/configs/test_attr_interface.py
@@ -0,0 +1,31 @@
+from pbutils.configs import get_config
+
+
+def test_attr_interface():
+    conf_data = '''
+[default]
+this = that
+these = those
+
+[orwell]
+war = peace
+ignorance = strength
+freedom = slavery
+'''
+    # conf = get_config('tests/configs/config.ini')
+    conf = get_config(conf_data)
+
+    assert hasattr(conf, 'default')
+    assert hasattr(conf, 'orwell')
+    assert not hasattr(conf, 'fart')
+
+    assert conf.default.this == 'that'
+    assert conf.default.these == 'those'
+    assert conf.this == 'that'
+    assert conf.these == 'those'
+
+    assert conf.orwell.war == 'peace'
+    assert conf.orwell.ignorance == 'strength'
+    assert conf.orwell.freedom == 'slavery'
+
+    assert not hasattr(conf.orwell, 'fart')
