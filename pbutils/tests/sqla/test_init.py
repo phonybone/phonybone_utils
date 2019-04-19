fbf08bf894ae9e44fb03b0ddd4e627e7b90e0c36
Victor Cassen <phonybone@gmail.com>
2019-04-18 21:54:13 -0700

    add tests/sqla_core/test_init.py

--- pbutils/tests/sqla/test_init.py
+++ pbutils/tests/sqla/test_init.py
@@ -5,11 +5,12 @@ from sqlalchemy.orm.session import sessionmaker
 from pbutils.sqla import sqlA_init_from_config_fn
 from pbutils.configs import get_config
 from pbutils.mysql_utils import get_mysql
-from pbutils.strings import qw, ppjson
 from pbutils.tests import create_db
+
 
 class SqlaTest(unittest.TestCase):
     def test_init(self):
+        ''' test initialization using sqlA_init_from_config_fn() '''
         mysql_ini = pr.resource_filename('pbutils', 'tests/fixtures/mysql.ini')
         config = get_config(mysql_ini)
         database = config.get('mysql', 'database')
