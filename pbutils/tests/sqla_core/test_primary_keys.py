ec74b447593ea86cea6d8097613d2d4a8a214808
Victor Cassen <phonybone@gmail.com>
2019-05-03 22:00:18 -0700

    test_primary_keys.py: working first attempt

---
+++ pbutils/tests/sqla_core/test_primary_keys.py
@@ -0,0 +1,15 @@
+from pbutils.sqla_core import init_sqla, SimpleStore, table_by_name, import_tables
+
+
+def test_primary_keys(sqlite_test_db_url):
+    engine, meta, conn = init_sqla(sqlite_test_db_url)
+    tables = import_tables(engine, meta)
+    print(F"tables: {tables}")
+
+    account_table = table_by_name(meta, 'account')
+    assert account_table is not None
+    store = SimpleStore(conn, account_table)
+    pks = store.primary_keys
+
+    for pk_col in pks:
+        assert pk_col.primary_key
