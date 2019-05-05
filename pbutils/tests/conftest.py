b8a748351ff3aa3b10274a06a9bcaf89fdaa6a95
Victor Cassen <phonybone@gmail.com>
2019-05-04 17:19:07 -0700

    Add conftest.py

--- pbutils/tests/conftest.py
+++ pbutils/tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from pbutils.sqla_core import init_sqla
+from pbutils.sqla_core import init_sqla, import_tables
 
 @pytest.fixture
 def sqlite_test_db_url():
@@ -8,10 +8,12 @@ def sqlite_test_db_url():
 
 @pytest.fixture
 def eng_meta_conn(sqlite_test_db_url):
+    ''' yields a tuple: engine, meta, and connection '''
     yield init_sqla(sqlite_test_db_url)
 
 
 @pytest.fixture
 def tables(eng_meta_conn):
+    engine, meta, conn = eng_meta_conn
     yield import_tables(engine, meta)
     
