38c0d7e02b4defe0f7aa5e87645961b432be42d3
Victor Cassen <c.victor.cassen@zonarsystems.com>
2019-05-20 10:30:02 -0700

    loggers.py: add time-format support for filename, cleanup

--- pbutils/loggers.py
+++ pbutils/loggers.py
@@ -3,17 +3,29 @@ import logging.handlers
 import os
 import sys
 
+from pbutils.times import ep2asc
+
+
 def create_module_logger(mod_name,
                          filename=None,
                          mode='w',
                          level=logging.DEBUG,
                          format_str='[%(module)s:%(filename)s:%(lineno)d] %(message)s'):
+    """
+    Create a logger dedicated to a particular module.
+    @param mod_name: name of the module (and sub-modules) to capture logging for
+    @param filename: name of file to store logs; defaults to mod_name
+    @mode: write-mode of file (use 'a' for append)
+    @level, @format_str: passed to logging config (level can be string or logging.INFO, etc)
+    """
     if not filename:
         # create a log file in the same directory as the module:
         module = sys.modules[mod_name]
         mod_dir = os.path.dirname(module.__file__)
         filename = os.path.abspath(os.path.join(mod_dir, '{}.log'.format(module.__name__)))
-        print('filename: {}'.format(filename))
+
+    if '%' in filename:
+        filename = ep2asc(fmt=filename)
 
     logger = logging.getLogger(mod_name)
     logger.propegate = False
@@ -21,7 +33,7 @@ def create_module_logger(mod_name,
     if isinstance(level, str):
         level = getattr(logging, level.upper())
     logger.setLevel(level)
-    
+
     handler = logging.FileHandler(filename, mode=mode, encoding='utf-8')
     logger.addHandler(handler)
 
@@ -30,9 +42,5 @@ def create_module_logger(mod_name,
     return logger
 
 if __name__ == '__main__':
-    #mod = sys.modules['loggers']
-    print('name: {}'.format(__name__))
-    #print('mod: {}'.format(mod))
     log = create_module_logger(__name__, mode='a')
     log.debug('Hi from {}'.format(__name__))
-              
