f4078d92bfda03f7632b90318d67ee1f6628cc1e
Victor Cassen <victor@statelessnetworks.com>
2020-10-04 21:41:15 -0700

    playing with magit: add docstring to StringEncoder

--- pbutils/strings.py
+++ pbutils/strings.py
@@ -72,6 +72,14 @@ def chunks(s, ll):
 
 
 class StringEncoder(json.JSONEncoder):
+    '''
+    Attempt to JSON encode anything.
+    First looks for an object method named 'as_json'
+    and invokes it if found.
+    Otherwise, attempts to convert sets to a string.
+    Otherwise, tries the default.  If that failes, returns
+    str(x)
+    '''
     def default(self, obj):
         if hasattr(obj, 'as_json'):
             return obj.as_json()
