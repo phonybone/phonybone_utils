6f99f7223b6dafee208173f31d7e7b2caa9875bd
Victor Cassen <phonybone@gmail.com>
2021-06-02 14:25:46 -0700

    dicts.py: add traverse_json, get_path_value, set_path_value, and is_scalar

--- pbutils/dicts.py
+++ pbutils/dicts.py
@@ -62,6 +62,62 @@ def json_to_object(data_str: str) -> SimpleNamespace:
     return json.loads(data_str, object_hook=lambda obj: SimpleNamespace(**obj))
 
 
+def traverse_json(data, path=None, only_leaves=False):
+    '''
+    Traverse json data; yield (path, value) for all paths
+    within data where path is a list of dict keys or list indexes
+    that "lead" to the value.
+    '''
+    if path is None:
+        path = []
+
+    if is_scalar(data):
+        yield path, data
+
+    elif isinstance(data, dict):
+        for key, value in data.items():
+            if not is_scalar(value) and not only_leaves:
+                yield path + [key], value
+            yield from traverse_json(value, path + [key], only_leaves)
+
+    elif isinstance(data, (list, tuple)):
+        for idx, value in enumerate(data):
+            if not is_scalar(value) and not only_leaves:
+                yield path + [idx], value
+            yield from traverse_json(value, path + [idx], only_leaves)
+
+    else:
+        raise TypeError(type(data))
+
+
+def get_path_value(data, path):
+    '''
+    Get the value in data indicated by path.
+    path is a list of keys/indexes.
+    Raise IndexError or KeyError as necessary.
+    '''
+    root = data
+    for elem in path:
+        root = root[elem]
+    return root
+
+
+def set_path_value(data, path, value):
+    '''
+    Set the value in data indicated by path.
+    path is a list of keys/indexes.
+    Raise IndexError or KeyError as necessary.
+    '''
+    root = data
+    for elem in path[:-1]:
+        root = root[elem]
+    root[path[-1]] = value
+
+
+def is_scalar(thing):
+    return isinstance(thing, (str, bytes, int, float, bool))
+
+
 if __name__ == '__main__':
     def test_remove_nones():
         d = dict(this='that', these='those', n=None)
