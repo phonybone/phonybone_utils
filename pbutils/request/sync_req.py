d23d416b39a453825aca404af6d5a0089bcad103
Victor Cassen <phonybone@gmail.com>
2021-09-15 16:20:57 -0700

    sync_req.py: refactor run_sync(), run_profiles()

--- pbutils/request/sync_req.py
+++ pbutils/request/sync_req.py
@@ -1,16 +1,48 @@
 from typing import List
+import json
 import requests
-from pbutils.request.utils import expand_profiles
+from pbutils.request.utils import create_request_params, all_contexts, populate_profile
 
 
-def run(profiles: List[dict], environ=None):
+def run_sync(profiles: List[dict]):
+    for response in run_profiles(profiles):
+        try:
+            content = response.json()
+            content = json.dumps(content, indent=4)
+        except Exception:
+            content = response.text
+
+        if 200 <= response.status_code <= 299:
+            output = content
+        else:
+            output = F"{response.request.url}: {response.status_code}\n{content}"
+
+        if hasattr(response, 'output_path'):
+            with open(response.output_path, 'w') as output_stream:
+                print(output, file=output_stream)
+                print(F"{response.output_path} written")
+        else:
+            print(output)
+
+
+def run_profiles(profiles: List[dict], environ=None):
+    '''
+    Run each profile once for each generated context.
+    As a side effect, persist the response as requested in profile.
+
+    Yield each response.
+    '''
     if isinstance(profiles, dict):
         profiles = [profiles]
     with requests.Session() as session:
-        for req_params in expand_profiles(profiles, environ):
-            yield do_request(req_params, session)
+        for profile in profiles:
+            for context in all_contexts(profile, environ):
+                profile = populate_profile(profile, context)
+                req_params = create_request_params(profile)
+                response = session.request(**req_params)
 
+                output_path = profile.pop('output_path', None)
+                if output_path:
+                    response.output_path = output_path
 
-def do_request(req_params, session):
-    ''' Issue request synchronously; return json or content '''
-    return session.request(**req_params)
+                yield response
