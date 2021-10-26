e97b8f3dc8ac65f3fe1974d51ae2e784fd252dd4
Victor Cassen <victor.cassen@zonarsystems.com>
2021-10-26 08:28:20 -0700

    sync_req.py: factor to handle_response() (ease of use for caller)

--- pbutils/request/sync_req.py
+++ pbutils/request/sync_req.py
@@ -6,25 +6,8 @@ from pbutils.request.logs import log
 
 
 def run_sync(profiles: List[dict]):
-    for response in run_profiles(profiles):
-        try:
-            content = response.json()
-            content = json.dumps(content, indent=4)
-        except Exception:
-            content = response.text
-
-        if 200 <= response.status_code <= 299:
-            log.debug(F"{response.request.url}: {response.status_code}")
-            output = content
-        else:
-            output = F"{response.request.url}: {response.status_code}\n{content}"
-
-        if hasattr(response, 'output_path'):
-            with open(response.output_path, 'w') as output_stream:
-                print(output, file=output_stream)
-                print(F"{response.output_path} written")
-        else:
-            print(output)
+    for profile, response in run_profiles(profiles):
+        yield profile, response
 
 
 def run_profiles(profiles: List[dict], environ=None):
@@ -44,8 +27,29 @@ def run_profiles(profiles: List[dict], environ=None):
                 req_params = create_request_params(pprofile)
                 response = session.request(**req_params)
 
-                output_path = pprofile.pop('output_path', None)
-                if output_path:
-                    response.output_path = output_path
+                # output_path = pprofile.pop('output_path', None)
+                # if output_path:
+                #     response.output_path = output_path
 
-                yield response
+                yield profile, response
+
+
+def handle_response(profile, response, config):
+    try:
+        content = response.json()
+        content = json.dumps(content, indent=4)
+    except Exception:
+        content = response.text
+
+    if not response.ok:
+        log.debug(F"{response.request.url}: {response.status_code}")
+        output = content
+    else:
+        output = F"{response.request.url}: {response.status_code}\n{content}"
+
+    if 'output_path' in profile:
+        with open(profile['output_path'], 'w') as output_stream:
+            print(output, file=output_stream)
+            log.debug(F"{response.output_path} written")
+    elif config.verbose:
+        print(output)
