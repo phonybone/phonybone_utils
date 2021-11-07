f743555c82a2bcdc32c045f73d3ff225b018e99c
Victor Cassen <phonybone@gmail.com>
2021-11-07 15:42:15 -0800

    request/logs.py: add format

--- pbutils/request/logs.py
+++ pbutils/request/logs.py
@@ -1,5 +1,6 @@
 import logging
 
-logging.basicConfig()
+fmt = "%(levelname)s %(threadName)s %(filename)s:%(lineno)s %(message)s"
+logging.basicConfig(format=fmt)
 log = logging.getLogger('arequest')
 log.setLevel(logging.INFO)
