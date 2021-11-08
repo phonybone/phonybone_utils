f6c0b666c7f44e6eeb860c395e463d77e73163de
Victor Cassen <phonybone@gmail.com>
2021-11-07 16:16:34 -0800

    threaded_rec.py: better loop management using th.Event()

--- pbutils/request/threaded_rec.py
+++ pbutils/request/threaded_rec.py
@@ -9,14 +9,22 @@ import requests
 from pbutils.request.utils import all_contexts, populate_profile, create_request_params
 
 class ThreadedReq:
+    '''
+    Threaded requests.
+    '''
     def __init__(self, n_threads, config, environ):
+        ''' constructor '''
         self.n_threads = n_threads
         self.config = config
         self.environ = environ
         self.inq = Q.Queue()
         self.outq = Q.Queue()
+        self.done = th.Event()
 
     def run(self, profiles):
+        '''
+        Process all profiles: use do_responses to obtain results.
+        '''
         for profile in profiles:
             for context in all_contexts(profile, self.environ):
                 pprofile = populate_profile(profile, context)
@@ -31,8 +39,13 @@ class ThreadedReq:
 
         for thread in threads:
             thread.join()
+        self.done.set()
 
     def do_requests(self):
+        '''
+        Read from inq, make the request, and put profile & response
+        onto outq.
+        '''
         while True:
             try:
                 profile, req_params = self.inq.get(block=False)
@@ -42,9 +55,14 @@ class ThreadedReq:
             self.outq.put((profile, resp))
 
     def do_responses(self):
+        '''
+        Yield all responses as they occur.
+        '''
         while True:
             try:
-                profile, resp = self.outq.get(timeout=3)
+                profile, resp = self.outq.get(timeout=3)  # to=3 a hack?
                 yield profile, resp
+                if self.outq.empty() and self.done.is_set():
+                    break
             except Q.Empty:
                 return
